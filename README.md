15c07165aff9814f7025c5e963d0443b395977fb
Gareth Kirk <gary.kirk@outlook.com>
2020-05-31 23:51:47 +0100

    Completed project

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 How to run this project:
 
+**You will need to run the below steps in your command line utility. You will need Node / npm installed to continue**
+
 1. Clone the repository from Github to your PC
    > git clone https://github.com/GarethTheNerd/Project-6-Static-Node.js-and-Express-Site.git
 2. Change into the folder containing the project
@@ -9,4 +11,10 @@ How to run this project:
    > npm install
 4. Run the app
    > npm start
-5. Access it in the browser at: http://localhost:300/
+5. Access it in the browser at: http://localhost:3000/
+
+Style changes made to this project:
+
+1. Changed font to Montserrat (loaded from Google's font API)
+2. Added underline when hovering over links
+3. Changed link color to orange
--- app.js
+++ app.js
@@ -1,31 +1,50 @@
-const express = require('express');
-const data = require('./data.json');
-
-//console.log(data);
-
+//Define our data object variable and require express.
+const express = require("express");
+const data = require("./data.json");
+//Create a new express app
 const app = express();
+//Define the port number so it can be changed easily
 const port = 3000;
 
-app.set('view engine', 'pug');
-
-app.use('/static', express.static('public'));
+app.set("view engine", "pug"); //Set the view engine for our pug files
+app.use("/static", express.static("public")); //Map /static to the public folder for images, css and js
 
 app.get("/", (req, res) => {
-    res.render("index");
+  //Setup index route
+  res.render("index", { projects: data.projects }); //Render the homepage and pass it project data so the homepage can show these dynamically
 });
 
 app.get("/about", (req, res) => {
-    res.render("about");
+  //Setup about route to render about template
+  res.render("about");
 });
 
 app.get("/projects/:id", (req, res) => {
-    const projectId = req.params.id;
-    
-    const projectObject = data.projects[projectId];
-
-    res.render("project", {project: projectObject});
+  const projectId = req.params.id; //get the project id from the dynamic route
+  const projectObject = data.projects[projectId]; //get the correct project from data object
+  res.render("project", { project: projectObject }); //render the project template and pass the project object
 });
 
+//I am setting the favicon like this as you get an error in the console otherwise
+app.get("/favicon.ico", (req, res) => {
+  res.sendFile("./G.ico", { root: __dirname }); //You can just send some text here but I would rather have a nice looking favicon that I made
+});
+
+//If we get this far without finding a route, we can't find the page
+app.use((req, res, next) => {
+  const err = new Error("Four Oh Four! Page not found"); //Define a error object
+  err.status = 404; //Set the status code
+  console.log("A page cannot be found!"); //Log a message to the console
+  next(err); //Pass it to our next middleware
+});
+
+//This function is used to render the error. We set the error to be 500 if not defined and pass it to the error template for rendering
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render("error", { err });
+});
+
+//Start the app listening on the port defined at the top of the file. We also log it out to the console.
 app.listen(port, () => {
-    console.log(`App is running on port ${port}`);
+  console.log(`App is running on port ${port}`);
 });
